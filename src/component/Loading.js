72c151a4cd987106ad52cde78b542fbb8a4e4e48
mrdevill <mrdevill@localhost.localdomain>
2019-09-01 20:10:07 +0100

    season discover

---
+++ src/component/Loading.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Loading = (props) => {
+    return (
+       
+    <div class="ui active dimmer">
+        <div class="big ui text loader">{props.message}</div>
+    </div>
+    )
+}
+
+Loading.defaultProps = {
+    message: 'Loading ...'
+};
+
+export default Loading;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import SeasonDisplay from './component/SeasonDisplay';
+import Loading from './component/Loading';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+class App extends React.Component{
+
+    state = {lat: null, errorMessage: ''};
+
+    componentDidMount(){
+        
+        window.navigator.geolocation.getCurrentPosition(
+                position=> this.setState({ lat: position.coords.latitude }), 
+                err => this.setState({errorMessage: err.message})
+            )
+            
+    }
+
+    renderContent(){
+
+        if (this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        if (!this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+        return <Loading message="Please accept location request" />
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            {this.renderContent()}
+            </div>
+            
+            )
+        
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
